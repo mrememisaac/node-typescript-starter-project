9331e291592367acb1b5d2f26fef39c496c01e63
Emem Isaac <me@ememisaac.com>
2019-12-15 18:14:37 +0100

    Write todo create function
    Starts #4

---
+++ src/controllers/TodoController.ts
@@ -0,0 +1,47 @@
+import {Request, Response } from 'express';
+import mongoose from 'mongoose';
+import Todo from '../models/Todo';
+import winston from 'winston';
+
+const errorMessages = { 
+    create: "An error occured while creating the todo", 
+    update: "An error occured while updating the resource", 
+    get: "An error occured while fetching the resource", 
+    delete: "An error occured while deleting the resource"
+}
+const logTypes = { error: "error", info: "info" }
+
+export class TodoController {
+    constructor(){}
+
+    public create(req: Request, res: Response){
+        
+        const required = new Map();
+        required.set("title", "Title");
+        required.set("dueDate", "Due date");
+        let message;
+
+        for(const [key, value] of required.entries()){
+            if(!req.body[key]){
+                message = `${value} is required`;
+                break;
+            }
+        }
+        if(message){
+            winston.log(logTypes.error, message, { request: req});
+            res.status(400).json({error: message });
+            return;
+        }
+        let newTask = new Todo(req.body);
+        newTask.save((err, task) => {
+            if(err){
+                res.status(500).send(errorMessages.create);
+                winston.log(logTypes.error, err, { request: req});
+                return;
+            }
+            res.json(task);
+        })
+    }
+
+    
+}
